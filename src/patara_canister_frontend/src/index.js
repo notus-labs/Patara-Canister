ce17424a303c8adef08bf0cbdb7f34a3ec433325
Mehmet Kırçal <mehmet@notuslabs.org>
2023-10-14 20:24:19 +0300

    Initial commit.

---
+++ src/patara_canister_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { patara_canister_backend } from "../../declarations/patara_canister_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await patara_canister_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
